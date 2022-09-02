98c4ae5fc7c23de720285aacdc3f31e4a87c6733
drftyuyin5 <111601541+drftyuyin5@users.noreply.github.com>
2022-09-02 20:56:00 +0200

    f

--- template-parts/footer-content.php
+++
@@ -1,5 +0,0 @@
-<footer class="u-align-center u-clearfix u-footer u-grey-80 u-footer" id="sec-386a">
-  <div class="u-clearfix u-sheet u-sheet-1">
-    <p class="u-small-text u-text u-text-variant u-text-1" data-lang-en="Sample text. Click to select the Text Element.">Swat Client 2021-2022°</p>
-  </div>
-</footer>
